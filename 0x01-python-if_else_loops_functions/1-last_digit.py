b119fc8e69096996a1a92cb2e07b49fa63422969
Paulette Wacheke <paulettekinguri@gmail.com>
2023-10-04 16:43:09 +0000

    assigns a random signed number to a variable number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+# Code gets the last digit of the number
+last_digit = abs(number) % 10
+
+# function determines if the last digit is greater than 5, 0, or less than 6 and not 0
+if number < 0:
+    last_digit *= -1
+
+# code prints the output based on the conditions
+    print(f"Last digit of {number} is {last_digit} and", end=" ")
+
+if last_digit > 5:
+    print("is greater than 5")
+elif last_digit == 0:
+    print("is 0")
+else:
+    print("is less than 6 and not 0")
