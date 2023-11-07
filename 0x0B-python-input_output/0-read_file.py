f36225eca364f0b2b0d128cd6b3632bc0e0f4daa
Paulette Wacheke <paulettekinguri@gmail.com>
2023-11-07 17:38:55 +0000

    Task 0.Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line, end='')
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
